470751886bdf0573c076d015c50f880c46f23ea7
MrLijan <liramj21@gmail.com>
2022-03-09 00:37:46 +0200

    [DOCKER] docker is working, HMR need to be fixed

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,3 +17,16 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+mix.options({
+    hmrOptions: {
+        host: 'localhost',
+        port: '8080'
+    }
+})
+
+mix.webpackConfig({
+    devServer: {
+        port:8000
+    }
+})
